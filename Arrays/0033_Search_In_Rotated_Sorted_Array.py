aef82afe8f1466fc0d5630181d8eec8d935eb442
anastasijabroceta <anastasija.broceta@gmail.com>
2026-07-23 19:15:54 +0200

    Add solutions for problem #33-modified binary search

---
+++ Arrays/0033_Search_In_Rotated_Sorted_Array.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            # Leva polovina je sortirana
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+               #desna polovina je sortirana
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+
+if __name__ == "__main__":
+     nums = [4, 5, 6, 7, 0, 1, 2]
+     target=0
+     sol = Solution()
+     print(sol.search( nums, target))
+     nums = [4, 5, 6, 7, 0, 1, 2]
+     target=3
+     sol = Solution()
+     print(sol.search( nums, target))
