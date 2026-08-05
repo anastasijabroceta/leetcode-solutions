b6e641a7be978de1f030b588c767cae074179b4f
anastasijabroceta <anastasija.broceta@gmail.com>
2026-08-05 19:36:08 +0200

    Add solution for problem  #1143-Dynamic Programming/2d matrix

---
+++ DP/1143_Longest_Common_Subsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        rows, cols = len(text1), len(text2)
+        dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+        for i in range(rows - 1, -1, -1):
+            for j in range(cols - 1, -1, -1):
+
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(
+                        dp[i + 1][j],
+                        dp[i][j + 1]
+                    )
+
+        return dp[0][0]
+if __name__ == '__main__':
+    print(Solution().longestCommonSubsequence(
+        text1="abc",
+        text2="a"
+    ))
